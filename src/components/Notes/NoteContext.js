a89cadf74f4f3306ff98c729d45436ba86e12027
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-06-23 13:02:51 +0800

    adding new NOTE_ACTION to reducer

--- src/components/Notes/NoteContext.js
+++ src/components/Notes/NoteContext.js
@@ -1,7 +1,6 @@
 import React, { useReducer, useContext } from "react";
-import produce from "immer";
+import produce, { original } from "immer";
 import { getDocNotes } from "./NoteData";
-
 
 // CONTEXTS
 export const NoteContext = React.createContext()
@@ -20,7 +19,8 @@ export function useUpdateNote() {
 export const NOTE_ACTION = {
     ADD_NOTE:'add-note',
     REMOVE_NOTE: 'remove-note',
-    UPDATE_NOTE: 'update-note'
+    UPDATE_NOTE: 'update-note',
+    MAKE_CHILD_NOTE: 'make-child-note'
 }
 function reducer(note, action) {
     switch (action.type) {
@@ -42,29 +42,33 @@ function reducer(note, action) {
             })
             return noteCopy     // return the edited copy
         case NOTE_ACTION.REMOVE_NOTE:
-            return produce(note, draft => {
-                // Initialize variables
+            let noteCopy2 = produce(note, draft => {
+                // Initialize Root variables
                 let path = action.data.path
                 let draftRef = draft
                 let noteIndx = action.data.indx
 
-                if (!action.data.isLastChild) {
-                    // loop through the parents until reaching the desired note container
+                if (action.data.isFirstChild && !action.data.isLastChild) {
+                    let NoteTextAppend = draft
+
+                    // Assigning the note container and note parent
                     for (let i = 0; i < path.length; i+=2) {
-                        if (i === path.length-2) draftRef[path[i]].content += action.data.noteText
+                        if (i === path.length-2) NoteTextAppend = draftRef[path[i]]
                         draftRef = draftRef[path[i]][path[i+1]]
-                        
                     }
 
-                    // Insert the child notes if note has childNotes
-                    if (action.data.hasChildren) draftRef.splice(noteIndx+1, 0, ...draftRef[noteIndx].insideNote)
+                    // add text to notetextappend.content
+                    NoteTextAppend.content += draftRef[noteIndx].content
+                    
+                    // if draftRef insideNote exists replace draftRef by its children
+                    if (draftRef[noteIndx].insideNote)  draftRef.splice(noteIndx, 1, ...draftRef[noteIndx].insideNote)
+                    // else remove draftRef
+                    else draftRef.splice(noteIndx, 1)
 
-                    // delete the note
-                    draftRef.splice(noteIndx, 1)
-                } else {
+                } else if (action.data.isLastChild && path.length > 0) {
                     // Initialize variables
-                    let insertPath = path.slice(0, -2)
-                    let notePath = path.slice(-2)
+                    let insertPath = path.length === 0 ? [] : path.slice(0, -2)      // [noteIndx, 'insideNote']
+                    let notePath = path.length === 0 ? [noteIndx] : path.slice(-2)
 
                     // loop through parents of insert path until reaching the desired note container
                     for (let i = 0; i < insertPath.length; i+=2) draftRef = draftRef[insertPath[i]][insertPath[i+1]]
@@ -73,13 +77,36 @@ function reducer(note, action) {
                     let noteRef = draftRef[notePath[0]].insideNote
 
                     // Insert the note to the container
-                    draftRef.splice(path.slice(-2)[0]+1, 0, {...noteRef[noteIndx]})
+                    if (noteRef) draftRef.splice(path.slice(-2)[0]+1, 0, {...noteRef[noteIndx]})
 
                     // delete the note
                     noteRef.splice(noteIndx, 1)
+                    draftRef[notePath[0]].insideNote = noteRef < 1 ? null : noteRef
+                } else {
+                    // Assigning the note container and note parent
+                    for (let i = 0; i < path.length; i+=2) draftRef = draftRef[path[i]][path[i+1]]
+
+                    // Retrieve the last child of previous note if it exist
+                    let NoteTextAppend = draftRef[noteIndx-1]
+                    let NoteTextAppendInside = NoteTextAppend.insideNote
+                    while (NoteTextAppendInside) {
+                        NoteTextAppend = [].slice.call(NoteTextAppend.insideNote).pop()
+                        NoteTextAppendInside = NoteTextAppend.insideNote
+                    }
+                 
+                    // add text to notetextappend.content
+                    NoteTextAppend.content += draftRef[noteIndx].content
+
+                    // if draftRef insideNote exists replace draftRef by its children
+                    if (draftRef[noteIndx].insideNote) draftRef.splice(noteIndx, 1, ...draftRef[noteIndx].insideNote)
+                    // else delete the note
+                    else draftRef.splice(noteIndx, 1)
+
                 }
+                return draft
 
             })
+            return noteCopy2
         case NOTE_ACTION.UPDATE_NOTE:
             return produce(note, draft => {
                 // Initialize variables
@@ -95,6 +122,31 @@ function reducer(note, action) {
                 // Update content
                 draftRef.content = action.data.newValue
             })
+        case NOTE_ACTION.MAKE_CHILD_NOTE: 
+            return produce(note, draft => {
+                // let pathToNote, pathToPrevNote
+                let contPath = action.data.contPath
+                let noteIndx = action.data.indx
+                let noteCont = draft
+
+                // loop through path notes and assign the path to note and prevNote
+                for (let i = 0; i < contPath.length; i+=2) {
+                    noteCont = noteCont[contPath[i]][contPath[i+1]]
+                }
+                console.log('prevNote: ', original(noteCont[noteIndx-1]))
+                console.log('currNote: ', original(noteCont[noteIndx]))
+
+                let prevNote = noteCont[noteIndx-1]
+                let currNote = noteCont[noteIndx]
+
+                // if prevNote.insideNote is null then assign [{...note}]
+                if (!prevNote.insideNote) prevNote.insideNote = [{...currNote}]
+                // else push {...note} to prevNote.insideNote
+                else prevNote.insideNote.push({...currNote})
+
+                // delete note
+                noteCont.splice(noteIndx, 1)
+            })
         default:
             console.log('ERROR: something went wrong in the reducer function')
             return note
@@ -105,9 +157,13 @@ function reducer(note, action) {
 export function NoteProvider({ children }) {
     const [note, setNote] = useReducer(reducer, getDocNotes())
 
+    function setMidNote(action) {
+        setNote(action)
+    }
+
     return (
         <NoteContext.Provider value={note}>
-            <UpdateNoteContext.Provider value={setNote}>
+            <UpdateNoteContext.Provider value={setMidNote}>
                 {children}
             </UpdateNoteContext.Provider>
         </NoteContext.Provider>
