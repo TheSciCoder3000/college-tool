23d1b52d4cebe33ad84b6b8a1fe9795cc00ff7ae
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-06-28 00:48:25 +0800

    file watcher feature update

--- src/components/FolderSys/FolderSystem.jsx
+++ src/components/FolderSys/FolderSystem.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { useState, useMemo } from 'react'
+import React, { useState, useMemo, useEffect } from 'react'
+import { setFolderOpen } from '../Notes/store'
 import File from './File'
 import Folder from './Folder'
+import StartWatcher, { WATCHER_EVENTS } from './watcher'
 
 const fs = window.require('fs')
 const pathModule = window.require('path')
-
 const { app } = window.require('@electron/remote')
+var watcher = null;
 
 const FileFolder = () => {
-    const [path, setPath] = useState(app.getAppPath())
+    const path = pathModule.join(app.getPath('userData'), 'NotesHome')
 
-    const files = useMemo(() => fs
+    // if userData folder does not exist then create folder
+    if (!fs.existsSync(path)) fs.mkdir(path, (error) => { if (error) console.error(error) })
+    const [fileSync, setFileSync] = useState(true)
+
+    // initialize files
+    let files = useMemo(() => fs
         .readdirSync(path)
         .map(file => {
             const stat = fs.statSync(pathModule.join(path, file))
-
+            
             return {
                 name: file,
                 type: stat.isFile() ? 'file' : 'folder',
@@ -24,12 +30,31 @@ const FileFolder = () => {
         })
         .sort((a, b) => {
             if (a.type === b.type) {
-              return a.name.localeCompare(b.name)
+                return a.name.localeCompare(b.name)
             }
             return a.type === 'folder' ? -1 : 1
-          }), [path])
+        }), [fileSync, path])
+    
+    // initializing the file watcher
+    useEffect(() => {
+        if (watcher) watcher.close()
+        watcher = StartWatcher(path, (event, data) => {
+            switch(event){
+                case WATCHER_EVENTS.FOLDER_DELETED:
+                    setFolderOpen(data.folderPath, false, true)
+                    break;
+            }
+            setFileSync(syncState => !syncState)
+        })
 
-    console.log('files: ', files)
+        // cleanup function
+        return () => {
+            // close watcher if component is unmounted
+            if (watcher) watcher.close()
+            watcher = null
+        }
+    }, [path])
+
 
     return (
         <div className="folder-sidepanel">
@@ -38,12 +63,14 @@ const FileFolder = () => {
             </div>
             <div className="folder-tree">
                 {files && (
-                    files.map((file, fileIndx) => {
-                        if (file.type == 'folder') return (
-                            <Folder key={fileIndx} folderData={file} />
-                        )
+                    files.map((file) => {
+                        if (file.type == 'folder') {
+                            return (
+                                <Folder key={`folder-${file.name}`} folderData={file} />
+                            )
+                        }
                         return (
-                            <File key={fileIndx} fileData={file} />
+                            <File key={`file-${file.name}`} fileData={file} />
                         )
                     })
                 )}
