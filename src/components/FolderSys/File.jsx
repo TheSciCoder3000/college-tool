b5ab5929c02a1929f5c190d6af983c981130cdb8
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-06-27 19:36:10 +0800

    file states and dbClick handlers

--- src/components/FolderSys/File.jsx
+++ src/components/FolderSys/File.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useOpenNote } from '../Notes/Note'
 
 const File = ({ fileData }) => {
+    // initialize states
     const [fileName, setFileName] = useState(fileData.name)
+    useEffect(() => setFileName(fileData.name), [fileData])
+    
+    const openNote = useOpenNote()
 
     const dbClick = (e) => {
         console.log(`file ${fileName} was clicked`)
+        openNote(fileData.path, fileData.name)
     }
 
     return (
