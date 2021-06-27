53328112572724032cacd38216c13ce4fc6288c0
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-06-27 13:22:09 +0800

    added file watcher

---
+++ src/components/FolderSys/watcher.js
@@ -0,0 +1,39 @@
+const chokidar = window.require('chokidar')
+var showInLogFlag = false;
+
+export default function StartWatcher(path, syncFiles){
+    let watcher = chokidar.watch(path, {
+        ignored: /[\/\\]\./,
+        persistent: true
+    });
+
+    function onWatcherReady(){
+        console.info('From here can you check for real changes, the initial scan has been completed.');
+        showInLogFlag = true;
+    }
+
+    watcher
+    .on('add', function(path) {
+        // console.log('File', path, 'has been added');
+        syncFiles()
+    })
+    .on('addDir', function(path) {
+        //  console.log('Directory', path, 'has been added');
+         syncFiles()
+     })
+    .on('change', function(path) {
+        // console.log('File', path, 'has been changed');
+        syncFiles()
+    })
+    .on('unlink', function(path) {
+        // console.log('File', path, 'has been removed');
+        syncFiles()
+    })
+    .on('unlinkDir', function(path) {
+        // console.log('Directory', path, 'has been removed');
+        syncFiles()
+    })
+    .on('ready', onWatcherReady)
+    
+    return watcher
+}
