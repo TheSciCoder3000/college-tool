96c1b9d84a31fa778d8aa727e9119e488f6f15fa
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-07-05 19:33:35 +0800

    typing bug fix and keyDown bug fix

--- src/components/Notes/ContentEditable.js
+++ src/components/Notes/ContentEditable.js
@@ -5,38 +5,48 @@ import { noteDataContext } from './NoteRow';
 export default class NoteContentEditable extends Component {
     constructor(props) {
         super(props);
+        this.textRef = React.createRef(this.props.text)
+        this.textRef.current = this.props.text
         this.state = {
-            text: this.props.text,
             keyDown: this.props.keyDown,
             textChange: this.props.onTextChange,
-            noteData: this.props.noteData
+            noteData: this.props.noteData,
+            sync: false
         }
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.text !== prevState.text) {
-            console.log('text change')
-            return({...prevState, text: nextProps.text})
-        }
         if (nextProps.noteData !== prevState.noteData) {
             return {...prevState, noteData: nextProps.noteData}
         }
         return null
     }
 
+    componentDidUpdate(prevProps) {
+        if (this.props.text !== this.textRef.current && this.textRef.current == prevProps.text) {
+            this.textRef.current = this.props.text
+            this.setState(currState => {
+                return { ...currState, sync: true }
+            })
+        }
+    }
+
     onTextChangeHandler(e) {
-        console.log('onChange', this.contentEditable)
         if (e.target.value === '<br>') {
-            this.setState({...this.state, text: ""})
+            this.textRef.current = ''
             this.state.textChange("", this.props.path, this.contentEditable)
         } else {
-            this.setState({...this.state, text: e.target.value})
+            this.textRef.current = e.target.value
             this.state.textChange(e.target.value, this.props.path, this.contentEditable)
+            
         }
     }
 
     onKeyDownHandler(e) {
-        this.state.keyDown(e, this.props.indx, this.props.path, this.state.text, this.state.noteData)
+        let permittedKeyCodes = [8, 9, 13, 38, 40]
+        if (permittedKeyCodes.includes(e.keyCode)){
+            this.state.keyDown(e, this.props.indx, this.props.path, this.state.text, this.state.noteData)
+        }
     }
 
     render() {
@@ -46,10 +56,13 @@ export default class NoteContentEditable extends Component {
                     className="note-content"
                     data-testid={`note-content-${this.props.noteId}`}
                     data-placeholder="type '/' for commands"
-                    html={this.state.text} 
+                    html={this.textRef.current} 
 
-                    onChange={this.onTextChangeHandler.bind(this)}
-                    onKeyDown={this.onKeyDownHandler.bind(this)} />
+                    // onSelect={(e) => console.log(document.getSelection())}
+                    
+                    tagName={'div'}
+                    onKeyDown={this.onKeyDownHandler.bind(this)}
+                    onChange={this.onTextChangeHandler.bind(this)} />
         )
     }
 }
