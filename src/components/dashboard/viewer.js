641df7114df5816a25f04709f7a547966e3c630f
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-07-11 14:33:53 +0800

    using animation variant file

--- src/components/dashboard/viewer.js
+++ src/components/dashboard/viewer.js
@@ -2,23 +2,10 @@ import '../../assets/css/dashboard/home.css'
 import LiveClock from "react-live-clock";
 import { NavLink as Link, useHistory } from 'react-router-dom';
 import { useCallback } from 'react';
+
 import { motion } from 'framer-motion';
+import { DashboardVariants } from '../../AnimationVariants';
 
-
-const ViewerVariant = {
-    hidden: {
-        opacity: 0
-    },
-    visible: {
-        opacity: 1
-    },
-    exit: {
-        opacity: 0,
-        transtion: {
-            duration: 1
-        }
-    }
-}
 
 const Viewer = ({ showSidePanel }) => {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -30,19 +17,9 @@ const Viewer = ({ showSidePanel }) => {
         history.push('/Calendar')
     }, [history])
 
-    const sampleSchedule = {
-        id: 0,
-        name: 'Physics 2',
-        time: '3:00 pm - 4:00 pm',
-    }
-
-    function switchSched() {
-        console.log('sched is being switched')
-    }
-
     return (
         <motion.div className="viewer"
-            variants={ViewerVariant}
+            variants={DashboardVariants.Viewer}
             initial='hidden'
             animate='visible'
             exit='exit'
