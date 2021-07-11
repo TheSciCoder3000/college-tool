6ccda55af4a8befc6564cf1a162d8837d5829be5
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-07-11 14:38:17 +0800

    adding dashboard animations

--- src/components/Routes/DashRoute.jsx
+++ src/components/Routes/DashRoute.jsx
@@ -1,20 +1,28 @@
 import Viewer from "../dashboard/viewer";
 import SidePanel from "../dashboard/SidePanel";
 
-import React, { useState } from 'react'
-import { AnimatePresence } from "framer-motion";
+import React, { useState, useEffect } from 'react'
+import { AnimatePresence, usePresence } from "framer-motion";
 
 const DashRoute = () => {
     document.onkeydown = null
     console.log('rendering')
+    const [isPresent, safeToRemove] = usePresence()
     const [ShowSidePanel, setShowSidePanel] = useState(false)
+    useEffect(() => {
+        if (!isPresent && !ShowSidePanel) safeToRemove()
+    }, [ShowSidePanel, isPresent])
     return (
         <>
             {/* tool viewer*/}
             <Viewer showSidePanel={setShowSidePanel} />
 
             {/* side panel */}
-            <AnimatePresence>
+            <AnimatePresence
+                onExitComplete={() => {
+                    if (!isPresent) safeToRemove()
+                }}
+            >
                 {ShowSidePanel && (<SidePanel />)}
             </AnimatePresence>
         </>
